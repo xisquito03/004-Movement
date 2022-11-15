702639b6845d3483c87eca20c5a85de2a3e60c77
xisquito03 <xisco034@gmail.com>
2022-11-15 13:07:58 +0100

    lol

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    public float turnSpeed = 10f;
-    public float horizontalInput;
+    private float speed = 20f;
+    private float turnSpeed = 10f;
+    private float horizontalInput;
+    private float verticalInput;
+
     private void Update()
     {
         // Detectar el Input Horizonal
         horizontalInput = Input.GetAxis("Horizontal");
+        // Detectar el Input Vertical
+        verticalInput = Input.GetAxis("Vertical");
 
-        // Movimiento hacia adelante
+        // Movimiento hacia adelante manual
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
 
         // Movimiento lateral manual
-        transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+       /* transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput); */
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+
+
+
     }
 }
